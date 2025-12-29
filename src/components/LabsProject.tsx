672fba329c9c2bd3e59fb2d237990f9cb8a56aa7
setPedro <pedross4789@gmail.com>
2025-12-29 23:17:45 +0100

    feat: add hover opacity to labs project links with colon exclusion

--- src/components/LabsProject.tsx
+++ src/components/LabsProject.tsx
@@ -9,10 +9,11 @@ export function LabsProject({ id, info }: LabsProject) {
     return (
         <div className="px-5 py-4">
             <Link href={`https://github.com/setpedro/${id}`}>
-                <span className="text-accent font-semibold whitespace-nowrap">
+                <span className="text-accent hover:text-accent/60 font-semibold whitespace-nowrap transition-all duration-100">
                     [ <span className="text-2xl leading-none">&#8599;</span>{" "}
-                    {id} ]:
+                    {id} ]
                 </span>
+                <span className="text-accent font-semibold">:</span>
             </Link>
             <span className="ml-1">{info}</span>
         </div>
